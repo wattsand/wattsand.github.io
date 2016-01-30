88583335893c4e0c0636f0b43c138e867e1684ce
wattsand <andreawatts22@gmail.com>
2016-01-30 16:21:13 -0600

    HTML/Pseudo Coede

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+//when user clicks on items in nav panel, redirect to 
+//correspinding portion of page
+
+//keep top nav fixed to top when scrolling
+
+//Keep contact information fixed on the right when scrolling
+
+//hide information under contact info phone and letter
+
+//when user clicks on contact information (phone, letter)
+//show corresponding information
+
+//when user clicks on download pdf, open new window with pdf resume
