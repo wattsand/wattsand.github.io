4353adf90cf9c4a1552ea5681f1a5e1e52848803
wattsand <andreawatts22@gmail.com>
2016-02-17 23:45:41 -0600

    Final Project Comitt (1)

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,112 @@
-//when user clicks on items in nav panel, redirect to 
-//correspinding portion of page
+// refresh
+$(window).on('beforeunload', function() {
+   $(window).scrollTop(0);
+});
 
-//keep top nav fixed to top when scrolling
+// scroll button
+var amountScrolled = 300;
 
-//Keep contact information fixed on the right when scrolling
+$(window).scroll(function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+    	
+        $('a.back-to-top').fadeIn('slow');
+        
+    } else {
+        $('a.back-to-top').fadeOut('slow');
+        
+    }
+});
 
-//hide information under contact info phone and letter
+$('a.back-to-top').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, 700); // speed of transition //
+    return false;
+});
 
-//when user clicks on contact information (phone, letter)
-//show corresponding information
+//scroll nav-bar
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.nav ul').outerHeight();
 
-//when user clicks on download pdf, open new window with pdf resume
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.nav ul').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.nav ul').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
+//tumblr captions
+// $('li.tumblr_post.tumblr_photo_post').on('click', function(){
+// 	$('.tumblr_caption p').show("slide",  { direction: "left" }, 1000);
+// });
+
+$('li.tumblr_post.tumblr_photo_post img').click(function() {
+	var thisUrl = $(this).next('.tumblr_caption').find('a:first').attr('href');
+	console.log(thisUrl);
+    window.location.href = thisUrl;
+});
+
+//contact info
+$(document).ready(function(){
+	$('.contact li').hide();
+});
+
+$('.contact h3').on('click', function(){
+	$('.contact li').slideToggle({ direction: "left" }, 1000);
+});
+
+//hamburger
+$('.hamburger').on('click', function (){
+
+	 // $('.nav ul').toggleClass('active');
+	 $('.nav ul').slideToggle('expand');
+	 
+});
+
+
+
+//possible side nav
+// $(document).ready(function(){
+// 	$('.side-nav li').hide();
+// });
+
+// $('.side-nav p').on('click', function(){
+// 	$('.side-nav li').show("slide", { direction: "left" }, 1000);
+// });
+
+// after::'.side-nav'{
+// 	content:'.background-image';
+// }
+
+
+
